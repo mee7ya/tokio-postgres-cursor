0d4f305e959bacee6ea85506b008f4d85c9c52da
mee7ya <bas9339@gmail.com>
2025-11-02 20:48:06 +0100

    closing + multithread + rng cursor name

--- src/cursor/stream.rs
+++ src/cursor/stream.rs
@@ -1,17 +1,24 @@
 use std::{
     pin::Pin,
-    rc::Rc,
-    task::{Context, Poll},
+    sync::Arc,
+    task::{Context, Poll, ready},
+};
+
+use rand::{
+    distr::{Alphanumeric, Distribution},
+    rng,
 };
 
 use futures_core::Stream;
+use tokio::runtime::Handle;
 use tokio_postgres::{Error, Row, Transaction};
 
 pub struct CursorStream<'a> {
-    tx: Rc<&'a Transaction<'a>>,
-    cursor: Rc<String>,
+    tx: Arc<&'a Transaction<'a>>,
+    cursor: Arc<String>,
     batch_size: usize,
     future: Option<Pin<Box<dyn Future<Output = Result<Vec<Row>, Error>> + 'a>>>,
+    done: bool,
 }
 
 impl<'a> CursorStream<'a> {
@@ -20,18 +27,28 @@ impl<'a> CursorStream<'a> {
         query: &str,
         batch_size: usize,
     ) -> Result<Self, Error> {
-        let cursor = "purrrsor".to_string();
+        let cursor = format!(
+            "cursor-{}",
+            Alphanumeric
+                .sample_iter(rng())
+                .take(3)
+                .map(|x| x as char)
+                .collect::<String>()
+        );
         tx.execute(&format!("DECLARE {} CURSOR FOR {}", cursor, query), &[])
             .await?;
+
         Ok(Self {
-            tx: Rc::new(tx),
-            cursor: Rc::new(cursor),
+            tx: Arc::new(tx),
+            cursor: Arc::new(cursor),
             batch_size,
             future: None,
+            done: false,
         })
     }
 
-    pub async fn close(self) -> Result<u64, Error> {
+    pub async fn close(mut self) -> Result<u64, Error> {
+        self.done = true;
         self.tx
             .execute(&format!("CLOSE {}", self.cursor), &[])
             .await
@@ -42,9 +59,13 @@ impl<'a> Stream for CursorStream<'a> {
     type Item = Result<Vec<Row>, Error>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        if self.done {
+            return Poll::Ready(None);
+        }
+
         if self.future.is_none() {
-            let tx = Rc::clone(&self.tx);
-            let cursor = Rc::clone(&self.cursor);
+            let tx = Arc::clone(&self.tx);
+            let cursor = Arc::clone(&self.cursor);
             let batch_size = self.batch_size;
 
             let future = Box::pin(async move {
@@ -58,8 +79,8 @@ impl<'a> Stream for CursorStream<'a> {
             self.future = Some(future);
         }
 
-        match self.future.as_mut().unwrap().as_mut().poll(cx) {
-            Poll::Ready(Ok(rows)) => {
+        match ready!(self.future.as_mut().unwrap().as_mut().poll(cx)) {
+            Ok(rows) => {
                 self.future = None;
                 if rows.is_empty() {
                     Poll::Ready(None)
@@ -67,11 +88,23 @@ impl<'a> Stream for CursorStream<'a> {
                     Poll::Ready(Some(Ok(rows)))
                 }
             }
-            Poll::Ready(Err(e)) => {
+            Err(e) => {
                 self.future = None;
                 Poll::Ready(Some(Err(e)))
             }
-            Poll::Pending => Poll::Pending,
+        }
+    }
+}
+
+impl Drop for CursorStream<'_> {
+    fn drop(&mut self) {
+        if !self.done {
+            Handle::current().block_on(async {
+                let _ = self
+                    .tx
+                    .execute(&format!("CLOSE {}", self.cursor), &[])
+                    .await;
+            });
         }
     }
 }
