ace57d12bc28b3a64d643ae963f1ce54be2a27b3
mee7ya <bas9339@gmail.com>
2025-11-03 22:17:53 +0100

    docs + cursor name fix + no scroll

--- src/cursor/ext.rs
+++ src/cursor/ext.rs
@@ -2,7 +2,15 @@ use tokio_postgres::{Error, Transaction};
 
 use crate::cursor::stream::CursorStream;
 
+/// Extension trait for 
+/// [`Transaction`](https://docs.rs/tokio-postgres/latest/tokio_postgres/struct.Transaction.html) 
+/// to add cursor support.
 pub trait TransactionExt {
+    /// Method to create a new [`CursorStream`] for the given query.
+    /// 
+    /// Parameters:
+    /// - `query`: The SQL query for which the cursor will be declared.
+    /// - `batch_size`: The number of rows to fetch in each batch.
     fn query_cursor<'a>(
         &'a self,
         query: &str,
@@ -12,7 +20,19 @@ pub trait TransactionExt {
         Self: 'a;
 }
 
+/// Implementation of [`TransactionExt`] for 
+/// [`Transaction`](https://docs.rs/tokio-postgres/latest/tokio_postgres/struct.Transaction.html)
 impl<'t> TransactionExt for Transaction<'t> {
+    /// Method to create a new [`CursorStream`] for the given query.
+    /// 
+    /// Parameters:
+    /// - `query`: The SQL query for which the cursor will be declared.
+    /// - `batch_size`: The number of rows to fetch in each batch.
+    /// 
+    /// Errors:
+    /// - Propagates 
+    /// [`tokio_postgres::Error`](https://docs.rs/tokio-postgres/latest/tokio_postgres/error/struct.Error.html) 
+    /// if the cursor declaration fails
     async fn query_cursor<'a>(
         &'a self,
         query: &str,
--- src/cursor/stream.rs
+++ src/cursor/stream.rs
@@ -13,6 +13,7 @@ use futures_core::Stream;
 use tokio::runtime::Handle;
 use tokio_postgres::{Error, Row, Transaction};
 
+/// A stream that fetches rows from a PostgreSQL cursor in batches.
 pub struct CursorStream<'a> {
     tx: Arc<&'a Transaction<'a>>,
     cursor: Arc<String>,
@@ -22,21 +23,35 @@ pub struct CursorStream<'a> {
 }
 
 impl<'a> CursorStream<'a> {
+    /// Creates a new [`CursorStream`] and declares a cursor for the given query.
+    /// 
+    /// Parameters:
+    /// - `tx`: A reference to the transaction in which the cursor will be declared.
+    /// - `query`: The SQL query for which the cursor will be declared.
+    /// - `batch_size`: The number of rows to fetch in each batch.
+    /// 
+    /// Errors:
+    /// - Propagates 
+    /// [`tokio_postgres::Error`](https://docs.rs/tokio-postgres/latest/tokio_postgres/error/struct.Error.html) 
+    /// if the cursor declaration fails.
     pub(crate) async fn new(
         tx: &'a Transaction<'a>,
         query: &str,
         batch_size: usize,
     ) -> Result<Self, Error> {
         let cursor = format!(
-            "cursor-{}",
+            "cursor_{}",
             Alphanumeric
                 .sample_iter(rng())
                 .take(3)
                 .map(|x| x as char)
                 .collect::<String>()
         );
-        tx.execute(&format!("DECLARE {} CURSOR FOR {}", cursor, query), &[])
-            .await?;
+        tx.execute(
+            &format!("DECLARE {} NO SCROLL CURSOR FOR {}", cursor, query),
+            &[],
+        )
+        .await?;
 
         Ok(Self {
             tx: Arc::new(tx),
@@ -47,6 +62,12 @@ impl<'a> CursorStream<'a> {
         })
     }
 
+    /// Closes the cursor associated with this stream.
+    /// 
+    /// Errors:
+    /// - Propagates 
+    /// [`tokio_postgres::Error`](https://docs.rs/tokio-postgres/latest/tokio_postgres/error/struct.Error.html) 
+    /// if the cursor closing fails
     pub async fn close(mut self) -> Result<u64, Error> {
         self.done = true;
         self.tx
